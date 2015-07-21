a4e8943d836214d69e1c8fc84501f4f0db959e98
perma <perma@users.noreply.github.com>
2015-07-21 12:33:27 +0200

    Preparations to add alternative in-memory db config.

---
+++ src/main/java/com/lundgbg/skrofs/config/db/DbConfiguration.java
@@ -0,0 +1,43 @@
+package com.lundgbg.skrofs.config.db;
+
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Profile("default")
+@Configuration
+public class DbConfiguration {
+
+	@Value("${jdbc.driverClassName}")
+	private String jdbcDriverClassName;
+
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+
+	@Value("${jdbc.username}")
+	private String jdbcUsername;
+
+	@Value("${jdbc.password}")
+	private String jdbcPassword;
+
+	@Bean(destroyMethod = "close")
+	public DataSource createPooledDataSource() throws PropertyVetoException {
+		final ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+		dataSource.setDriverClass(jdbcDriverClassName);
+		dataSource.setJdbcUrl(jdbcUrl);
+		dataSource.setUser(jdbcUsername);
+		dataSource.setPassword(jdbcPassword);
+		dataSource.setMaxStatements(180);
+
+		return dataSource;
+	}
+
+}
